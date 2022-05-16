fa8117f21db21f0206069fae629ea0d5242342a9
csuchorski <cezary.suchorski@wp.pl>
2022-05-16 09:28:12 +0200

    Added methods for connecting to a hub, mapped correctly

---
+++ TicTacToe/wwwroot/js/gamepage.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/connect").build();
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("Connected");
+    }
+    catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
+
+connection.onclose(() => {
+    console.log("Disconnected")
+})
+
+start();
