b18993d583d2f712f123aa84f3caf0375adedea9
csuchorski <cezary.suchorski@wp.pl>
2022-05-18 10:43:24 +0200

    Added method for testing if the game is won

--- TicTacToe/Models/GameController.cs
+++ TicTacToe/Models/GameController.cs
@@ -2,24 +2,47 @@
 {
     public class GameController
     {
-        private int [,] board = new int[3,3];
-        public int [,] Board
+        private int[,] board = new int[3, 3];
+        public int[,] Board
         {
             get { return board; }
             set { board = value; }
-        } 
+        }
 
         public bool IsMoveValid(byte x, byte y)
         {
+            if (Board[x, y] != 0)
+            {
+                return false;
+            }
             return true;
         }
 
-        public void MakeMove(byte x, byte y, bool piece)
+        public void MakeMove(byte x, byte y, byte piece)
         {
-
+            Board[x, y] = piece;
         }
         public bool IsMoveWinning()
         {
+            if (Board[0,0] + Board[0,1] + Board[0,2] == 3) return true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    sum+=Board[i,j];
+                }
+                if (sum == 3 || sum == 6) return true;
+                sum = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    sum += Board[j, i];
+                }
+                if (sum == 3 || sum == 6) return true;
+            }
+            if (Board[0, 0] + Board[1, 1] + Board[2, 2] == 3 || Board[0, 0] + Board[1, 1] + Board[2, 2] == 6) return true;
+            if (Board[2, 0] + Board[1, 1] + Board[0, 2] == 3 || Board[2, 0] + Board[1, 1] + Board[0, 2] == 6) return true;
 
             return false;
         }
